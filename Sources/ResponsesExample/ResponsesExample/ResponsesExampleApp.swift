90d3eabda660116f9af95c7af1d784c6925ae2a2
Ronald Mannak <ronaldmannak@me.com>
2025-10-05 10:33:56 -0700

    Add example project

---
+++ Sources/ResponsesExample/ResponsesExample/ResponsesExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsesExampleApp.swift
+//  ResponsesExample
+//
+//  Created by Ronald Mannak on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ResponsesExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
